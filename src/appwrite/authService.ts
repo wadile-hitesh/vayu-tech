e76663abed0359bc7e41d94a1ec0d4a39c7e6055
Hitesh Wadile <hiteshwadile.dev@gmail.com>
2024-06-01 15:50:35 +0530

    add appwrite auth service

--- src/appwrite/authService.ts
+++ src/appwrite/authService.ts
@@ -1,5 +1,5 @@
 import conf from "@/conf/config";
-import { Account, Appwrite, Client, ID } from "appwrite";
+import { Account, Client, ID } from "appwrite";
 
 interface CreateUserAccount {
   email: string;
@@ -21,5 +21,67 @@ appwriteClient
 export const account = new Account(appwriteClient);
 
 class AuthService {
+
   // Create User Account
+
+  async createUserAccount({email ,password, name}: CreateUserAccount){
+    try {
+      const userAccount = await account.create(ID.unique(), email, password, name);
+
+      if(userAccount){
+        return this.loginUser({email,password});
+      }
+      else{
+        return userAccount;
+      }
+    } catch (error) {
+      console.log("Error: Sign Up Error", error);
+    }
+  } 
+
+
+  // Login User
+  async loginUser({email, password}: LoginUser){
+    try {
+      return await account.createSession(email, password);
+    }
+    catch (error) {
+      console.log("Error: Login Error", error);
+    }
+  }
+
+
+  // Check if User is Logged In
+  async isUserLoggedIn() : Promise<boolean>{
+    try {
+      const user = await this.getCurrentUser();
+      return Boolean(user);
+    } catch (error) {
+        console.log("Error: User Not Logged In", error);
+    }
+    return false; 
+  }
+
+  // Get Current User
+  async getCurrentUser(){
+    try {
+      const user = await account.get();
+      return user;
+    } catch (error) {
+      console.log("Error: Get Current User", error);
+    }
+    return null;
+  }
+
+  // Logout User
+  async logoutUser(){
+    try {
+      return await account.deleteSession("current");
+    } catch (error) {
+      console.log("Error: Logout User", error);
+    }
+  }
 }
+
+const appwriteService = new AuthService();
+export default appwriteService;
